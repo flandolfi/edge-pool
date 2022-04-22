5fcc0bf4b6b94fac2d6fc21990631d23ced045f2
Francesco Landolfi <flandolfi@users.noreply.github.com>
2022-04-22 19:54:59 +0200

    Fix JIT

--- pool/utils.py
+++ pool/utils.py
@@ -2,13 +2,10 @@ import torch
 from torch_geometric.typing import Tensor, Union, SparseTensor
 
 
-def rank_edge_from_nodes(edge_index: Union[SparseTensor, Tensor], rank: Tensor) -> Tensor:
-    if isinstance(edge_index, SparseTensor):
-        row, col, _ = edge_index.coo()
-    else:
-        row, col = edge_index[0], edge_index[1]
-
+def rank_edge_from_nodes(adj: SparseTensor, rank: Tensor) -> Tensor:
+    row, col, _ = adj.coo()
     n = rank.size(0)
+
     row_rank, col_rank = rank[row], rank[col]
     return torch.minimum(row_rank*n + col_rank, col_rank*n + row_rank)
 
