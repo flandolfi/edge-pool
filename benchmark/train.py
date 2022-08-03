69d1d9ee6743d0b53cf5a7bdf50d353b2257c5b2
Francesco Landolfi <flandolfi@users.noreply.github.com>
2022-08-03 17:37:06 +0200

    Add MalNetTiny and update training script

--- benchmark/train.py
+++ benchmark/train.py
@@ -5,7 +5,7 @@ import math
 import os
 
 from torch_geometric.data.lightning_datamodule import LightningDataset
-from torch_geometric.datasets import TUDataset, MalNetTiny
+from torch_geometric.datasets import TUDataset
 from torch_geometric.transforms import Constant, Compose, ToUndirected
 from torch_geometric import seed_everything
 
@@ -22,6 +22,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 from benchmark import models
+from benchmark.datasets import MalNetTiny
 
 warnings.filterwarnings("ignore", category=Warning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -33,34 +34,38 @@ def get_datasets(dataset: str = 'DD',
                  num_workers: int = 0,
                  seed: int = 42):
     root = os.path.realpath(root)
-    
+
     with FileLock(os.path.expanduser('~/.data.lock')):
         if dataset in {'mal-net', 'MalNet', 'MalNetTiny'}:
-            dataset = MalNetTiny(root=os.path.join(root, 'MalNetTiny'),
-                                 transform=Compose([Constant(), ToUndirected()]))
+            root = os.path.join(root, 'MalNetTiny')
+            transform = Compose([Constant(), ToUndirected()])
+
+            train_dataset = MalNetTiny(root=root, split="train", transform=transform)
+            valid_dataset = MalNetTiny(root=root, split="val", transform=transform)
+            test_dataset = MalNetTiny(root=root, split="test", transform=transform)
         else:
             dataset = TUDataset(root=root, name=dataset)
-            
+
             if dataset.num_node_features == 0:
                 dataset.transform = Constant()
-    
-    idx = list(range(len(dataset)))
-    y = dataset.data.y.numpy()
-    
-    train_idx, test_idx = train_test_split(idx, test_size=0.2,
-                                           random_state=seed,
-                                           stratify=y)
-    train_idx, valid_idx = train_test_split(train_idx, test_size=0.125,
-                                            random_state=seed,
-                                            stratify=y[train_idx])
-    
-    train_dataset = dataset[train_idx]
-    valid_dataset = dataset[valid_idx]
-    test_dataset = dataset[test_idx]
-    
+
+            idx = list(range(len(dataset)))
+            y = dataset.data.y.numpy()
+
+            train_idx, test_idx = train_test_split(idx, test_size=0.2,
+                                                   random_state=seed,
+                                                   stratify=y)
+            train_idx, valid_idx = train_test_split(train_idx, test_size=0.125,
+                                                    random_state=seed,
+                                                    stratify=y[train_idx])
+
+            train_dataset = dataset[train_idx]
+            valid_dataset = dataset[valid_idx]
+            test_dataset = dataset[test_idx]
+
     return LightningDataset(train_dataset, valid_dataset, test_dataset,
                             batch_size=batch_size, num_workers=num_workers)
-    
+
 
 def train(model: str = 'Baseline',
           dataset: str = 'DD',
@@ -72,7 +77,7 @@ def train(model: str = 'Baseline',
           **trainer_kwargs):
     config = dict(config or {})
     batch_size = config.get('batch_size', 8)
-    
+
     if 'batch_size' in config:
         config.pop('batch_size')
 
@@ -85,10 +90,10 @@ def train(model: str = 'Baseline',
 
     trainer = pl.Trainer(**trainer_kwargs)
     trainer.fit(model, datamodule)  # noqa
-    
+
     if test:
         return trainer.test(datamodule=datamodule)  # noqa
-    
+
     return None
 
 
@@ -101,7 +106,8 @@ def grid_search(model: str = 'Baseline',
                 gpu_per_trial: float = 0,
                 verbose: int = 1,
                 refit: Union[bool, int] = 10,
-                seed: int = 42):
+                seed: int = 42,
+                **run_kwargs):
     logging_level = logging.INFO if verbose > 0 else logging.WARNING
     logging.basicConfig(format='%(levelname)s: %(message)s',
                         level=logging_level)
@@ -121,11 +127,11 @@ def grid_search(model: str = 'Baseline',
                 param_grid[k] = tune.grid_search(v)
             else:
                 param_grid[k] = v
-                
+
     root = os.path.realpath(root)
     reporter = CLIReporter(
-        metric_columns=["loss", "accuracy", "training_iteration"])
-    
+        metric_columns=["loss", "accuracy", "training_iteration", "test_acc"])
+
     def _train_partial(config):
         train(model, dataset, root, config,
               num_workers=cpu_per_trial, test=False,
@@ -154,26 +160,48 @@ def grid_search(model: str = 'Baseline',
                         progress_reporter=reporter,
                         raise_on_failed_trial=False,
                         fail_fast=False,
-                        name=exp_name)
+                        name=exp_name,
+                        **run_kwargs)
 
     logging.info(f"Best config:\t{analysis.best_config}\n")
-    
+
     if not refit:
         logging.info(f"Checkpoint at:\t{analysis.best_checkpoint}\n")
         return
 
-    results = []
     best_config = analysis.best_config
 
-    for test_seed in range(int(refit)):
+    def _test_partial(config):
         metric_list = train(model=model, dataset=dataset, root=root,
                             config=dict(best_config), num_workers=cpu_per_trial,
-                            gpus=math.ceil(gpu_per_trial), test=True, seed=test_seed)
-        results.append(metric_list[0])
+                            progress_bar_refresh_rate=0, gpus=math.ceil(gpu_per_trial),
+                            test=True, seed=config['seed'], callbacks=[
+                                TuneReportCheckpointCallback({
+                                    "loss": "val_loss",
+                                    "accuracy": "val_acc"
+                                }, on="validation_end")
+                            ])
+        tune.report(**metric_list[0])
+
+    logging.info("Starting tests...")
+    results = tune.run(_test_partial,
+                       resources_per_trial={
+                           'cpu': cpu_per_trial,
+                           'gpu': gpu_per_trial,
+                       },
+                       local_dir=local_dir,
+                       config={'seed': tune.grid_search(list(range(int(refit))))},
+                       num_samples=1,
+                       verbose=verbose,
+                       progress_reporter=reporter,
+                       raise_on_failed_trial=False,
+                       fail_fast=False,
+                       name=exp_name + '_test',
+                       **run_kwargs)
 
     logging.info(f"Model assessment results:\n\n{results}")
-    
-    df_results = pd.DataFrame.from_records(results)
+
+    df_results = results.results_df.set_index('config.seed')[['test_acc']]
     df_config = pd.DataFrame.from_records([best_config])
     results_path = os.path.join(local_dir, exp_name, 'model_assessment.json')
     config_path = os.path.join(local_dir, exp_name, 'best_config.json')
